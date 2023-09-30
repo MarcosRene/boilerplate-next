5e1807a5a5c0805f177438f948f38a4c899bbd38
MarcosRene <renemoura789@gmail.com>
2023-09-29 23:12:22 -0300

    feat: add styled-components

---
+++ src/Components/Main/styles.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  text-align: center;
+
+  background-color: #03a64a;
+  color: #ffffff;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
